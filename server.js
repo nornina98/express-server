e8d0fc01ba0bb1c41cd7f3713ae70f429669e900
Nor Ikram <nornina8998@gmail.com>
2024-03-06 16:04:12 +0800

    route and parameter for express

--- server.js
+++ server.js
@@ -4,8 +4,48 @@ const app = express();
 
 const PORT = 3000;
 
+const friends = [
+  {
+    id: 0,
+    name: "nor-ikram",
+  },
+  {
+    id: 1,
+    name: "nor-hakimin",
+  },
+  {
+    id: 2,
+    name: "nor-aiman",
+  },
+];
+
 app.get("/", (req, res) => {
-  res.send("HIIIIII");
+  res.send({
+    id: 1,
+    name: "nor-ikram",
+  });
+});
+
+app.get("/messages", (req, res) => {
+  res.send(`<ul>
+  <li>Sending HTML tagging into respond</li>
+  </ul>`);
+});
+
+app.get("/friends", (req, res) => {
+  res.json(friends);
+});
+
+app.get("/friends/:FriendId", (req, res) => {
+  const FriendId = Number(req.params.FriendId);
+  const id = friends[FriendId];
+  if (id) {
+    res.status(200).json(id);
+  } else {
+    res.status(404).json({
+      error: "Friend ID does not exist",
+    });
+  }
 });
 
 app.listen(PORT, () => {
